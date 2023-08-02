4c0ae6ef2ddf3acf93863d858943f499765c3436
sytrejo <s.y.trejo26@gmail.com>
2023-08-02 15:28:13 -0400

    updates

--- productDemo/src/app/products/product-detail/product-detail.component.ts
+++ productDemo/src/app/products/product-detail/product-detail.component.ts
@@ -1,21 +1,30 @@
-import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnChanges {
 
   //construstors should be relatively empty and devoid of logic other than setting initial variables.
   // Adding business logic inside a constructor makes it challenging to mock it in testing scenarios.
   constructor(){
     //console.log(`Name is ${this.name} in the constructor`);
   }
+
+  //implementing the OnChanges hook
+  ngOnChanges(changes: SimpleChanges): void {
+    const product = changes['name'];
+    const oldValue = product.previousValue;
+    const newValue = product.currentValue;
+    console.log(`Product changed from ${oldValue} to ${newValue}`);
+  }
+
+  //implementing the ngOnInit hook
   ngOnInit(): void {
     //A good use of the OnInit hook is when we need to initialize a component with data from an external source.
     //console.log(`Name is ${this.name} in the ngOnInit`);
-    throw new Error('Method not implemented.');
   }
 
   @Input() name = ' ';
--- productDemo/src/app/products/product-list/product-list.component.ts
+++ productDemo/src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    if(this.productDetail){
+      console.log(this.productDetail.name);
+
+    }
+  }
   selectedProduct = ' ';
 
   onBuy(name: string){
     window.alert(`You just bought ${name}!`);
   }
+
+  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent| undefined;
 
 }
