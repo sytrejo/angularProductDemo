1c275cf17e05e8af7b05647e7e5849d56cf6357c
sytrejo <s.y.trejo26@gmail.com>
2023-08-02 15:05:19 -0400

    working with console log to identify difference between constructor and onInit

--- productDemo/src/app/app.component.ts
+++ productDemo/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'productDemo';
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
 }
--- productDemo/src/app/products/product-detail/product-detail.component.ts
+++ productDemo/src/app/products/product-detail/product-detail.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+
+  //construstors should be relatively empty and devoid of logic other than setting initial variables.
+  // Adding business logic inside a constructor makes it challenging to mock it in testing scenarios.
+  constructor(){
+    //console.log(`Name is ${this.name} in the constructor`);
+  }
+  ngOnInit(): void {
+    //A good use of the OnInit hook is when we need to initialize a component with data from an external source.
+    //console.log(`Name is ${this.name} in the ngOnInit`);
+    throw new Error('Method not implemented.');
+  }
 
   @Input() name = ' ';
   @Output() bought = new EventEmitter<string>();
